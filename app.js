f108b338c57457532aa79d37608d863f0f2a068b
capt-aliengit config --global user.email eric.botcher@presidio.edu <ericbotcher@Aliens-MacBook-Air.local>
2018-09-20 15:08:14 -0700

    almost first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+var exphbs = require('express-handlebars');
+const express = require('express')
+const app = express()
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Hello World!' });
+})
+
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
