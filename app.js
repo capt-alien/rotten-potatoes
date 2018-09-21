a691f4a5e8eb8b8f9a5265153faff571d3a51e90
capt-aliengit config --global user.email eric.botcher@presidio.edu <ericbotcher@Aliens-MacBook-Air.local>
2018-09-21 16:20:57 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.get('/', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
 })
 
+//listen
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
@@ -45,14 +46,25 @@ app.listen(3000, () => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
-    console.log(err.message);
+    console.log(err.message)
   })
 })
 
 // NEW
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
+})
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+    // params => URL PARAMETERS
+    // console.log(req.params.taco)
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
