445627e8f971890ca75f100ec53727bb6deef691
capt-aliengit config --global user.email eric.botcher@presidio.edu <ericbotcher@Aliens-MacBook-Air.local>
2018-09-21 10:42:15 -0700

    user can see all reviews

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // app.js
+
 var exphbs = require('express-handlebars');
 const express = require('express')
 const app = express()
@@ -7,13 +8,18 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
-})
 
-// app.get('/', (req, res) => {
-//   res.render('home', { msg: 'Hello World!' });
-// })
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+// INDEX
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
