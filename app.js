ddfff125eb769f78ff4bcc4d0fbde11173f41c7c
capt-aliengit config --global user.email eric.botcher@presidio.edu <ericbotcher@Aliens-MacBook-Air.local>
2018-09-21 11:35:58 -0700

    added mongoDB and review Model

--- app.js
+++ app.js
@@ -9,14 +9,23 @@ app.set('view engine', 'handlebars');
 
 
 
-// OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
+// // OUR MOCK ARRAY OF PROJECTS
+// let reviews = [
+//   { title: "Great Review" },
+//   { title: "Next Review" }
+// ]
 
 // INDEX
-// INDEX
+app.get('/', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
 app.get('/', (req, res) => {
   res.render('reviews-index', { reviews: reviews });
 })
@@ -24,3 +33,12 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
+
+//connects to mongo database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+const Review = mongoose.model('Review', {
+  title: String
+});
